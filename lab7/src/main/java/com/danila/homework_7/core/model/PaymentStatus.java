f0cba08d4e52313b49d16db53d8ff3fdfe99f08c
Tr1bunal1218 <74443072+Tr1bunal1218@users.noreply.github.com>
2025-04-08 18:50:43 +0700

    features: add lab7

---
+++ lab7/src/main/java/com/danila/homework_7/core/model/PaymentStatus.java
@@ -0,0 +1,5 @@
+package com.danila.homework_7.core.model;
+
+public enum PaymentStatus {
+    PENDING, COMPLETED, REFUNDED, FAILED
+}
---
+++ lab7/src/main/java/com/danila/homework_7/core/model/value/Measurement.java
@@ -0,0 +1,6 @@
+package com.danila.homework_7.core.model.value;
+
+public interface Measurement {
+    String getName();
+    String getSymbol();
+}
